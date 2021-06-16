2a19b183573d8691c62bbeace234ec6a76e7ad6c
veera_1505 <veerasaraha@gmail.com>
2021-06-16 13:35:05 +0530

    added covid-19 table component and basic style

--- src/StateWiseTable.js
+++ src/StateWiseTable.js
@@ -1,9 +1,57 @@
 import React from 'react'
+import _ from 'lodash'
 
-const StateWiseTable = () => {
+const StateWiseTable = ({ data }) => {
+  const convertToNumberFormat = (data) => {
+    let result = data.map((state) => ({
+      id: _.uniqueId(),
+      state: state.loc,
+      confirmedCases: new Intl.NumberFormat('en-IN').format(
+        state.totalConfirmed
+      ),
+      indian: new Intl.NumberFormat('en-IN').format(state.confirmedCasesIndian),
+      foreigner: new Intl.NumberFormat('en-IN').format(
+        state.confirmedCasesForeign
+      ),
+      discharged: new Intl.NumberFormat('en-IN').format(state.discharged),
+      deaths: new Intl.NumberFormat('en-IN').format(state.deaths),
+    }))
+
+    return result
+  }
+
+  let covidData = convertToNumberFormat(data)
   return (
     <div>
-      <h2>Table</h2>
+      {!covidData ? (
+        'No data Found'
+      ) : (
+        <table className='table table-bordered'>
+          <thead>
+            <tr>
+              <th>state</th>
+              <th>totalConfirmed</th>
+              <th>confirmedCasesIndian</th>
+              <th>confirmedCasesForeign</th>
+              <th>discharged</th>
+              <th>deaths</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {covidData.map((data) => (
+              <tr key={data.id}>
+                <td>{data.state}</td>
+                <td>{data.confirmedCases}</td>
+                <td>{data.indian}</td>
+                <td>{data.foreigner}</td>
+                <td>{data.discharged}</td>
+                <td>{data.deaths}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
--- src/SummaryCard.js
+++ src/SummaryCard.js
@@ -10,30 +10,38 @@ const SummaryCard = ({ data }) => {
   } = data
 
   return (
-    <div>
-      <div>
-        <h2>Total Confirmed Cases</h2>
-        <span>{new Intl.NumberFormat('en-IN').format(total)}</span>
-      </div>
-      <div>
-        <h2>Total Confirmed Indians</h2>
-        <span>
-          {new Intl.NumberFormat('en-IN').format(confirmedCasesIndian)}
-        </span>
-      </div>
-      <div>
-        <h2>Total Confirmed Foreigners </h2>
-        <span>
-          {new Intl.NumberFormat('en-IN').format(confirmedCasesForeign)}
-        </span>
-      </div>
-      <div>
-        <h2>Total Discharged</h2>
-        <span>{new Intl.NumberFormat('en-IN').format(discharged)}</span>
-      </div>
-      <div>
-        <h2>Total Deaths</h2>
-        <span>{new Intl.NumberFormat('en-IN').format(deaths)}</span>
+    <div className='container-fluid text-center py-3 my-3 mx-auto'>
+      <div className='row mx-auto'>
+        <div className='col-6 p-2'>
+          <h2 className='display-6'>Total Confirmed Cases</h2>
+          <span className='display-6 py-2 px-2'>
+            {new Intl.NumberFormat('en-IN').format(total)}
+          </span>
+        </div>
+        <div className='col-6 p-2'>
+          <h2 className='display-6'>Total Confirmed Indians</h2>
+          <span className='display-6 py-2 px-2'>
+            {new Intl.NumberFormat('en-IN').format(confirmedCasesIndian)}
+          </span>
+        </div>
+        <div className='col-5 p-2'>
+          <h2 className='display-6'>Total Confirmed Foreigners </h2>
+          <span className='display-6 py-2 px-2'>
+            {new Intl.NumberFormat('en-IN').format(confirmedCasesForeign)}
+          </span>
+        </div>
+        <div className='col p-2'>
+          <h2 className='display-6'>Total Discharged</h2>
+          <span className='display-6 py-2 px-2'>
+            {new Intl.NumberFormat('en-IN').format(discharged)}
+          </span>
+        </div>
+        <div className='col p-2'>
+          <h2 className='display-6'>Total Deaths</h2>
+          <span className='display-6 py-2 px-2'>
+            {new Intl.NumberFormat('en-IN').format(deaths)}
+          </span>
+        </div>
       </div>
     </div>
   )
